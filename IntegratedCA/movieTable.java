eccf62d45da768c1826779fff0c43580746febaa
MorganeTassoneiro <2021327@student.cct.ie>
2023-12-08 19:06:39 +0000

    try and catch added

--- IntegratedCA/movieTable.java
+++ IntegratedCA/movieTable.java
@@ -7,14 +7,14 @@ import java.sql.Statement;
 public class MovieTableCreator {
     private final DatabaseConnector connector;
 
-    // CONSTRUCTOR
-    public MovieTableCreator(String databaseName, DatabaseConnector connector) {
+       public MovieTableCreator(DatabaseConnector connector, String databaseName) {
         this.connector = connector;
         this.databaseName = databaseName;
     }
 
-    public void createMoviesTable() {
-
+ public void createMoviesTable() {
+        String createTableSQL = generateCreateTableSQL();
+        
        // Create the movies table if it doesn't exist
             try (Statement statement = connection.createStatement()) {
                 statement.executeUpdate("CREATE TABLE IF NOT EXISTS movies (" +
@@ -23,6 +23,21 @@ public class MovieTableCreator {
                         "rented BOOLEAN NOT NULL" +
                         ")");
 
+         try (Connection connection = connector.getConnection(); 
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate(createTableSQL);
+            System.out.println("Successfully created.");
+        } catch (SQLException e) {
+            handleSQLException("An error was made: " + e.getMessage(), e);
             }
-    
+        }
+    }
 }
+    private void handleSQLException(String errorMessage, SQLException e) {
+        System.out.println(errorMessage);
+        e.printStackTrace();
+    }
+
+
+
+
